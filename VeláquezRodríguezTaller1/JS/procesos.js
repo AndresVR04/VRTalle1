f501bb6ea799d16e2f9bc781c07a5c39f993f1d1
AndresVR04 <90435994+AndresVR04@users.noreply.github.com>
2022-08-09 11:42:42 -0600

    Add files via upload

---
+++ VeláquezRodríguezTaller1/JS/procesos.js
@@ -0,0 +1,75 @@
+var aleatorio =  1+Math.floor(Math.random()*100);
+var cont = 1;
+var datos = [];
+
+// function comparar(){
+//     var numero = document.getElementById("numero").value;
+
+//     if(numero < 1 || numero > 100){
+//         document.getElementById("mensaje").innerHTML= "Escribir un numero dentro del rango...";
+//     }else if(numero == aleatorio){
+//         document.getElementById("mensaje").innerHTML="FELICIDADES, LO ENCONTRASTE!!!!";
+//     }else if(numero < aleatorio){
+//         document.getElementById("mensaje").innerHTML= "El numero es mayor al ingresado...";
+//         datos.push(numero);
+//         ++cont;
+//     }else if(numero > aleatorio){
+//         document.getElementById("mensaje").innerHTML= "El numero es menor al ingresado...";
+//         datos.push(numero);
+//         ++cont;
+//     }
+    
+    
+
+// }
+
+console.log(aleatorio);
+
+
+function inic(){
+
+    do {
+        numero = prompt("Digita el numero!!!");
+        if(numero != aleatorio){
+            cont += 1;
+            datos.push(numero);
+        }
+        
+    } while (numero != aleatorio);
+
+    if(numero < 1 || numero > 100){
+        document.getElementById("mensaje").innerHTML= "Escribir un numero dentro del rango...";
+    }else if(numero == aleatorio){
+        document.getElementById("mensaje").innerHTML="FELICIDADES, LO ENCONTRASTE!!!!";
+    }else if(numero < aleatorio){
+        document.getElementById("mensaje").innerHTML= "El numero es mayor al ingresado...";
+    }else if(numero > aleatorio){
+        document.getElementById("mensaje").innerHTML= "El numero es menor al ingresado...";
+
+    }
+
+
+    if(cont==1){
+        document.getElementById("nivel").innerHTML= "Sobresaliente";
+    }else if(cont > 1 && cont < 4){
+        document.getElementById("nivel").innerHTML= "Notable";
+    }else if(cont >= 4 && cont < 11){
+        document.getElementById("nivel").innerHTML= "Muy Bien";
+    }else if(cont >=11 && cont < 25){
+        document.getElementById("nivel").innerHTML= "Regular";
+    }else if(cont > 25){
+        document.getElementById("nivel").innerHTML= "Pésimo";
+    }
+
+
+
+}
+
+console.log(cont);
+console.log(datos.length);
+
+window.onload = inic();
+
+
+
+
